774e6c3f262c77578492f0af6f99525ba7c84e99
n1tuneyi <youssefhammam77@gmail.com>
2024-09-28 16:18:13 +0300

    First Commit

---
+++ Chapter 1/First Example/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace First_Example
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
