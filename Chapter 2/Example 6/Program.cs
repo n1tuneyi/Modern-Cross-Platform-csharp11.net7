6b7af3e572752661f1a19fa5164cab24c17ed535
n1tuneyi <youssefhammam77@gmail.com>
2024-09-29 13:32:58 +0300

    Trying Digit Separation with underscores

---
+++ Chapter 2/Example 6/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace Example_6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int hundredThousand = 100_000;
+            int million = 1_000_000;
+            int binaryNum = 0b_0000_1010; // this equals 10
+            Console.WriteLine($"{hundredThousand}, {million}, {binaryNum}");
+        }
+    }
+}
