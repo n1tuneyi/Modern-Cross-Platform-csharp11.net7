9fc893b9592b33ee271d29461a80552115853637
n1tuneyi <youssefhammam77@gmail.com>
2024-09-29 13:19:57 +0300

    Raw + Interpolated String Example

---
+++ Chapter 2/Example 5/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace Example_5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var person = new { FirstName = "Alice", Age = 56 };
+            string json = $$"""
+                            
+                            "first_name": "{{person.FirstName}}",
+                            "age": {{person.Age}},
+                            "calculation", "{{{1 + 2}}}"
+                            
+                            """;
+                Console.WriteLine(json);
+        }
+    }
+}
