a22e89d0ab5932833ca839e0459c57e53ea44d08
n1tuneyi <youssefhammam77@gmail.com>
2024-09-29 14:16:17 +0300

    Formatting Strings methods

---
+++ Chapter 2/Example 13/Program.cs
@@ -0,0 +1,18 @@
+﻿int numberOfApples = 12;
+
+decimal pricePerApple = 0.35M;
+
+
+Console.WriteLine(
+        format: "{0} apples cost {1:C}",
+        arg0: numberOfApples,
+        arg1: pricePerApple * numberOfApples
+    );
+
+string formatted = string.Format(
+                    format: "{0} apples cost {1:C}",
+                    arg0: numberOfApples,
+                    arg1: pricePerApple * numberOfApples
+                );
+
+Console.WriteLine(formatted);
