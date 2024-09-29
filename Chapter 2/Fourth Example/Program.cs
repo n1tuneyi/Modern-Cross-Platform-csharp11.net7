2e5bc1e7a8842e038297aa8b1056eaf60916c486
n1tuneyi <youssefhammam77@gmail.com>
2024-09-29 13:01:31 +0300

    trying string constructor

---
+++ Chapter 2/Fourth Example/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace Fourth_Example
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {  
+            string horizontalLine = new('-', count: 50);
+            Console.WriteLine(horizontalLine);
+
+        }
+    }
+}
