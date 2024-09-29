9da42371fdde67490754a962af0ca5993d3d2cff
n1tuneyi <youssefhammam77@gmail.com>
2024-09-29 12:41:17 +0300

    Global Using Imports

---
+++ Chapter 2/Third Example/GlobalUsing.cs
@@ -0,0 +1,10 @@
+﻿global using System;
+global using System.Linq;
+global using System.Collections.Generic;
+
+namespace Third_Example
+{
+    internal class GlobalUsing
+    {
+    }
+}
---
+++ Chapter 2/Third Example/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace Third_Example
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> list = [1, 2, 3];
+            Console.WriteLine(list.Where(num => num == 2).First());
+        }
+    }
+}
