202259babf0c224a52173796884adb708df2f4d2
n1tuneyi <youssefhammam77@gmail.com>
2024-09-29 13:51:13 +0300

    casting object types

---
+++ Chapter 2/Example 9/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace Example_9
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            object height = 1.73; 
+            object name = "Youssef"; 
+            Console.WriteLine($"{name} is {height} metres tall.");
+            //int length1 = name.Length; Object type doesn't have property Length 
+            int length2 = ((string)name).Length;  // Here we can access Length after casting to string
+            Console.WriteLine($"{name} has {length2} characters.");
+        }
+    }
+}
