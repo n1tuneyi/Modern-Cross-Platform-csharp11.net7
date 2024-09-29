a6ba189b222c47157583552e260d35cafc3cf885
n1tuneyi <youssefhammam77@gmail.com>
2024-09-29 14:29:11 +0300

    Reading input from console

---
+++ Chapter 2/Example 14/Program.cs
@@ -0,0 +1,7 @@
+﻿Console.Write("Type your first name and press enter: ");
+string? firstName = Console.ReadLine();
+
+Console.Write("Type your age and press enter: ");
+string? age = Console.ReadLine();
+
+Console.WriteLine($"Hello {firstName}, you look good for {age}.");
