b2842a870769acadec9f79d12ec95537dc3e619b
n1tuneyi <youssefhammam77@gmail.com>
2024-09-29 14:56:45 +0300

    using math operators

---
+++ Chapter 3/Example 1/Program.cs
@@ -0,0 +1,16 @@
+﻿int a = 1;
+int b = 2;
+
+WriteLine($"a: {a} b: {b}");
+
+WriteLine($"a + b = {a + b}");
+WriteLine($"a - b = {a - b}");
+WriteLine($"a * b = {a * b}");
+WriteLine($"a / b = {a / b}");
+WriteLine($"a % b = {a % b}");
+
+
+double c = 11.0;
+
+WriteLine($"a: {a} c: {c}");
+WriteLine($"a / c = {a / c}");
