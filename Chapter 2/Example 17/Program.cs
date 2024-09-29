22361454fbbd94e1cae8c38d8c84a4ce3323cd17
n1tuneyi <youssefhammam77@gmail.com>
2024-09-29 14:48:12 +0300

    Writing await in top level statements

---
+++ Chapter 2/Example 17/Program.cs
@@ -0,0 +1,6 @@
+﻿
+HttpClient client = new();
+
+HttpResponseMessage response = await client.GetAsync("http://www.apple.com/");
+
+WriteLine("Apple page has {0:N0} bytes.", response.Content.Headers.ContentLength);
