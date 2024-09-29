7964db9cef04518b6a6d5fdb986b66b2f1c14621
n1tuneyi <youssefhammam77@gmail.com>
2024-09-29 13:44:28 +0300

    Double variables problem with ==

---
+++ Chapter 2/Example 8/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace Example_8
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double a = 0.1;
+            double b = 0.2;
+            if(a + b == 0.3)
+                Console.WriteLine($"{a} + {b} = {0.3}");
+            else
+                Console.WriteLine($"{a} + {b} != {0.3}");
+        }
+    }
+}
