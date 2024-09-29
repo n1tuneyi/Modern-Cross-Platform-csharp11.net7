b14686cbeda32f51e1fc4091ea9a5dc3f65218a1
n1tuneyi <youssefhammam77@gmail.com>
2024-09-29 12:05:38 +0300

    Setting <LangVersion> to older version while using new features

---
+++ Chapter 2/First Example/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace First_Example
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            List<int> lst = [1, 2, 3, 4];
+        }
+    }
+}
