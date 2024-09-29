9a4866f7cdfe992608bc7d8ce3cff3837008de82
n1tuneyi <youssefhammam77@gmail.com>
2024-09-29 13:14:29 +0300

    multi line string feature

--- Chapter 2/Example Four/Program.cs
+++ Chapter 2/Example Four/Program.cs
@@ -7,10 +7,18 @@
             //string horizontalLine = new('-', count: 50);
             //Console.WriteLine(horizontalLine);
 
-            string nameWithTab = "youssef\tarek";
-            string actualNameWithoutTab = @"youssef\tarek";
-            Console.WriteLine(nameWithTab);
-            Console.WriteLine(actualNameWithoutTab);
+            //string nameWithTab = "youssef\tarek";
+            //string actualNameWithoutTab = @"youssef\tarek";
+            //Console.WriteLine(nameWithTab);
+            //Console.WriteLine(actualNameWithoutTab);
+
+            string story = """
+                this is a documentary about a hero called 
+                ben 10 and he has a watch that transforms him 
+                into one of 10 aliens on pressing
+                """;
+
+            Console.WriteLine(story);
         }
 
     }
