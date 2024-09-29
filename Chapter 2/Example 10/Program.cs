cacb66569b5e336334185f23ab6d2b0733f584e1
n1tuneyi <youssefhammam77@gmail.com>
2024-09-29 13:54:56 +0300

    Dynamic Keyword Problems

---
+++ Chapter 2/Example 10/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace Example_10
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            dynamic obj = "Youssef";
+
+            //obj = 30;
+
+            // Not safe because we don't know at runtime if obj is guaranteed to have Length property
+            Console.WriteLine($"Length is {obj.Length}");
+
+        }
+    }
+}
