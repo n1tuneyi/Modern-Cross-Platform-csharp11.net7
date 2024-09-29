e946952fe36f16a7d1d2748abee2277aefc99159
n1tuneyi <youssefhammam77@gmail.com>
2024-09-29 14:07:52 +0300

    Target-typed new() initialization shortcut

---
+++ Chapter 2/Example 12/Program.cs
@@ -0,0 +1,15 @@
+﻿Person person = new();
+
+person.FirstName = "Youssef";
+
+List<Person> persons = new()
+{
+    new() {FirstName = "Mohamed"},
+    new() {FirstName = "Mostafa"}
+};
+
+
+class Person
+{
+    public String FirstName;
+}
