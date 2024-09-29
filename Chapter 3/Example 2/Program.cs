1e054feb253583545eed4f1fdb231d8887bdc41f
n1tuneyi <youssefhammam77@gmail.com>
2024-09-29 15:07:47 +0300

    Test AND OR XOR on boolean

---
+++ Chapter 3/Example 2/Program.cs
@@ -0,0 +1,13 @@
+﻿bool a = true;
+bool b = false;
+WriteLine($"AND | a | b ");
+WriteLine($"a | {a & a,-5} | {a & b,-5} ");
+WriteLine($"b | {b & a,-5} | {b & b,-5} ");
+WriteLine();
+WriteLine($"OR | a | b ");
+WriteLine($"a | {a | a,-5} | {a | b,-5} ");
+WriteLine($"b | {b | a,-5} | {b | b,-5} ");
+WriteLine();
+WriteLine($"XOR | a | b ");
+WriteLine($"a | {a ^ a,-5} | {a ^ b,-5} ");
+WriteLine($"b | {b ^ a,-5} | {b ^ b,-5} ");
