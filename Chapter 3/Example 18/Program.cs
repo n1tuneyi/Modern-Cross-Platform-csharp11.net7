3927d881c472328be9f16029743ec316e21f2e62
n1tuneyi <youssefhammam77@gmail.com>
2024-10-03 10:51:35 +0300

    Checked and Unchecked with overflow scenarios

---
+++ Chapter 3/Example 18/Program.cs
@@ -0,0 +1,23 @@
+﻿checked
+{
+    int x = int.MaxValue - 1;
+    WriteLine($"Initial value: {x}");
+    x++;
+    WriteLine($"After incrementing: {x}");
+    x++;
+    WriteLine($"After incrementing: {x}"); 
+    x++;
+    WriteLine($"After incrementing: {x}");
+}
+
+
+unchecked
+{
+    int y = int.MaxValue + 1;
+    WriteLine($"Initial value: {y}");
+    y--;
+    WriteLine($"After decrementing: {y}");
+    y--;
+    WriteLine($"After decrementing: {y}");
+}
+
