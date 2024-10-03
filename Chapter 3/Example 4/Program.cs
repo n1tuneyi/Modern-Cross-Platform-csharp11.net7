9e25065368d7337f43fb7b2b7d3f30fddfdf9a92
n1tuneyi <youssefhammam77@gmail.com>
2024-10-03 07:04:49 +0300

    If statements

--- Chapter 3/Example 4/Program.cs
+++ Chapter 3/Example 4/Program.cs
@@ -2,3 +2,15 @@
 
 WriteLine($"The {nameof(age)} variable uses {sizeof(int)} bytes of memory.");
 
+
+String password = "ninja";
+
+
+if(password.Length < 8)
+{
+    WriteLine("Your password is too short. Use at least 8 characters.");
+}
+else
+{
+    WriteLine("Your password is strong.");
+}
