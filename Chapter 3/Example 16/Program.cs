1762931653fffe42524d3891216d532e39074cc6
n1tuneyi <youssefhammam77@gmail.com>
2024-10-03 09:58:13 +0300

    Parse and TryParse Methods

---
+++ Chapter 3/Example 16/Program.cs
@@ -0,0 +1,23 @@
+﻿int age = int.Parse("27");
+
+DateTime birthday = DateTime.Parse("4 July 1980");
+
+WriteLine($"I was born {age} years ago.");
+WriteLine($"My birthday is {birthday}.");
+WriteLine($"My birthday is {birthday:D}.");
+
+
+//int count = int.Parse("abc");
+
+Write("How many eggs are there? ");
+
+string? input = ReadLine();
+
+if(int.TryParse(input, out int count))
+{
+    WriteLine($"There are {count} eggs.");
+}
+else
+{
+    WriteLine("can't parse the input.");
+}
