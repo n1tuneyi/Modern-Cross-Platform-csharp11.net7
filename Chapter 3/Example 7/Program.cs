565b23bbe2b5ea5a9f586fc3982d94b959236919
n1tuneyi <youssefhammam77@gmail.com>
2024-10-03 07:20:43 +0300

    pattern matching using switch

---
+++ Chapter 3/Example 7/Program.cs
@@ -0,0 +1,35 @@
+﻿Animal?[] animals =
+[
+        new Cat { Name = "Karen", Born = new(year: 2022, month: 8, day: 23), Legs = 4, IsDomestic = true },
+        null,
+        new Cat { Name = "Mufasa", Born = new(year: 1994, month: 6, day: 12) },
+        new Spider { Name = "Sid Vicious", Born = DateTime.Today, IsPoisonous = true },
+        new Spider { Name = "Captain Furry", Born = DateTime.Today }
+];
+foreach (Animal? animal in animals)
+{
+    string message;
+    switch (animal)
+    {
+        case Cat fourLeggedCat when fourLeggedCat.Legs == 4:
+            message = $"The cat named {fourLeggedCat.Name} has four legs.";
+            break;
+        case Cat wildCat when wildCat.IsDomestic == false:
+            message = $"The non-domestic cat is named {wildCat.Name}.";
+            break;
+        case Cat cat:
+            message = $"The cat is named {cat.Name}.";
+            break;
+        default: // default is always evaluated last
+            message = $"The animal named {animal.Name} is a {animal.GetType().
+            Name}.";
+            break;
+        case Spider spider when spider.IsPoisonous:
+            message = $"The {spider.Name} spider is poisonous. Run!";
+            break;
+        case null:
+            message = "The animal is null.";
+            break;
+    }
+    WriteLine($"switch statement: {message}");
+}
