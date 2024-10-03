0ac8cdb824223a4210d0cfeabb67159b023420eb
n1tuneyi <youssefhammam77@gmail.com>
2024-10-03 10:45:15 +0300

    filters in catch statement

--- Chapter 3/Example 17/Program.cs
+++ Chapter 3/Example 17/Program.cs
@@ -20,3 +20,22 @@ catch (Exception ex)
     WriteLine($"{ex.GetType()} says {ex.Message}");
 }
 WriteLine("After parsing");
+
+
+
+Write("Enter an amount: ");
+string amount = ReadLine()!;
+if (string.IsNullOrEmpty(amount)) return;
+try
+{
+    decimal amountValue = decimal.Parse(amount);
+    WriteLine($"Amount formatted as currency: {amountValue:C}");
+}
+catch (FormatException) when (amount.Contains("$"))
+{
+    WriteLine("Amounts cannot use the dollar sign!");
+}
+catch (FormatException)
+{
+    WriteLine("Amounts must only contain digits!");
+}
