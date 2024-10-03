4ac14f3db443f026a643d49a230e0389408bea1d
n1tuneyi <youssefhammam77@gmail.com>
2024-10-03 07:46:42 +0300

    Single-dimension array

---
+++ Chapter 3/Example 9/Program.cs
@@ -0,0 +1,13 @@
+﻿string[] names = new[] { "Kate", "Jack", "Rebecca", "Tom" };
+//names = new string[4];
+
+names[0] = "Kate";
+names[1] = "Jack";
+names[2] = "Rebecca";
+names[3] = "Tom";
+
+for (int i = 0; i < names.Length; i++)
+{
+    
+    WriteLine(names[i]);
+}
