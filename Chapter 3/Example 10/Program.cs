3ffb9be5d5a674b4f809252a8aa457f8eebcbd74
n1tuneyi <youssefhammam77@gmail.com>
2024-10-03 07:52:31 +0300

    Multi dimensional array

---
+++ Chapter 3/Example 10/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+string[,] grid = new[,]
+{
+    { "Alpha", "Beta", "Gamma", "Delta" },
+    { "Anne", "Ben", "Charlie", "Doug" },
+    { "Aardvark", "Bear", "Cat", "Dog" }
+};
+
+WriteLine($"Lower bound of the first dimension is: {grid.GetLowerBound(0)}");
+WriteLine($"Upper bound of the first dimension is: {grid.GetUpperBound(0)}");
+WriteLine($"Lower bound of the second dimension is: {grid.GetLowerBound(1)}");
+WriteLine($"Upper bound of the second dimension is: {grid.GetUpperBound(1)}");
+
+for (int row = 0; row <= grid.GetUpperBound(0); row++)
+{
+    for (int col = 0; col <= grid.GetUpperBound(1); col++)
+    {
+        WriteLine($"Row {row}, Column {col}: {grid[row, col]}");
+    }
+}
+
+string[,] grid2 = new string[3, 4]; 
+grid2[0, 0] = "Alpha"; 
+grid2[0, 1] = "Beta";
+
+grid2[2, 3] = "Dog";
