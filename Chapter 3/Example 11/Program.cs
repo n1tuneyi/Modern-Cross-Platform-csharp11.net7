e9d40b4661fcfb66fd63733d1fcb907fd5cf0527
n1tuneyi <youssefhammam77@gmail.com>
2024-10-03 07:59:02 +0300

    jagged arrays

---
+++ Chapter 3/Example 11/Program.cs
@@ -0,0 +1,23 @@
+﻿string[][] jagged =
+[
+    new[] { "Alpha", "Beta", "Gamma" },
+    new[] { "Anne", "Ben", "Charlie", "Doug" },
+    new[] { "Aardvark", "Bear" }
+];
+
+WriteLine("Upper bound of array of arrays is: {0}", jagged.Length);
+
+for (int array = 0; array <= jagged.GetUpperBound(0); array++)
+{
+    WriteLine("Upper bound of array {0} is: {1}",
+    arg0: array,
+    arg1: jagged[array].GetUpperBound(0));
+}
+
+for (int row = 0; row <= jagged.GetUpperBound(0); row++)
+{
+    for (int col = 0; col <= jagged[row].GetUpperBound(0); col++)
+    {
+        WriteLine($"Row {row}, Column {col}: {jagged[row][col]}");
+    }
+}
