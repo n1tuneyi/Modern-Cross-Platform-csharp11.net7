bba70cbc9e7e169ca9fedea04704cbc57bf6d13d
n1tuneyi <youssefhammam77@gmail.com>
2024-10-03 09:35:52 +0300

    Implicit and explicit casting

---
+++ Chapter 3/Example 13/Program.cs
@@ -0,0 +1,14 @@
+﻿int a = 10;
+double b = a;
+WriteLine(b);
+
+double c = 9.8;
+int d = (int)c;
+WriteLine(d);
+
+long e = 10;
+int f = (int)e;
+WriteLine($"e is {e:N0} and f is {f:N0}");
+e = 5_000_000_000;
+f = (int)e;
+WriteLine($"e is {e:N0} and f is {f:N0}");
