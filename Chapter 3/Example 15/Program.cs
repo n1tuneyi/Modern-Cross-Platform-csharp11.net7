01e1a37bfda37ce5db30fe3f773680965f6c5fbc
n1tuneyi <youssefhammam77@gmail.com>
2024-10-03 09:50:55 +0300

    Converting types to String

---
+++ Chapter 3/Example 15/Program.cs
@@ -0,0 +1,30 @@
+﻿using static System.Convert;
+
+int number = 12;
+WriteLine(number.ToString());
+
+bool boolean = true;
+WriteLine(boolean.ToString());
+
+DateTime now = DateTime.Now;
+WriteLine(now.ToString());
+
+object me = new();
+WriteLine(me.ToString());
+
+byte[] binaryObject = new byte[128];
+
+Random.Shared.NextBytes(binaryObject);
+
+WriteLine("Binary Object as bytes:");
+
+for (int index = 0; index < binaryObject.Length; index++)
+{
+    Write($"{binaryObject[index]:X} ");
+}
+
+WriteLine();
+
+string encoded = ToBase64String(binaryObject);
+
+WriteLine($"Binary Object as Base64: {encoded}");
