08be91ca4ed4a8bec3daa624591d7c7ce54b1888
n1tuneyi <youssefhammam77@gmail.com>
2024-10-03 07:38:21 +0300

    iteration statements

---
+++ Chapter 3/Example 8/Program.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+
+int x = 0;
+
+while (x < 10)
+{
+    WriteLine(x);
+    x++;
+}
+
+string? password;
+
+do
+{
+    WriteLine("Enter your password: ");
+    password = ReadLine();
+} while (password != "Pa$$w0rd");
+WriteLine("Correct!");
+
+
+for (int i = 1;i <= 10;i++)
+{
+    WriteLine(i);
+}
+
+string[] names = { "Adam", "Barry", "Charlie" };
+
+foreach (string name in names)
+{
+    WriteLine($"{name} has {name.Length} characters.");
+}
+
+IEnumerator e = names.GetEnumerator();
+while (e.MoveNext())
+{
+    string name = (string)e.Current; 
+    WriteLine($"{name} has {name.Length} characters.");
+}
