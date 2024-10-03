310cbde8609d3dbabf49ace2b1501f37f3e06082
n1tuneyi <youssefhammam77@gmail.com>
2024-10-03 07:13:09 +0300

    Switch statement on int

---
+++ Chapter 3/Example 6/Program.cs
@@ -0,0 +1,23 @@
+﻿int number = Random.Shared.Next(1, 7);
+WriteLine($"My random number is {number}");
+switch (number)
+{
+    case 1:
+        WriteLine("One");
+        break;
+    case 2:
+        WriteLine("Two");
+        goto case 1;
+    case 3: 
+    case 4:
+        WriteLine("Three or four");
+        goto case 1;
+    case 5:
+        goto A_label;
+    default:
+        WriteLine("Default");
+        break;
+} 
+WriteLine("After end of switch");
+A_label:
+WriteLine($"After A_label");
