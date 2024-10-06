24f2336447ab9f40277ecfb3c5b66b551eb102f4
n1tuneyi <youssefhammam77@gmail.com>
2024-10-06 13:23:37 +0300

    Debugging tools

---
+++ Chapter 4/Example 5/Program.cs
@@ -0,0 +1,13 @@
+﻿
+double Add(double a , double b)
+{
+    return a + b;
+}
+
+double a = 4.5;
+double b = 2.5;
+double answer = Add(a, b);
+
+WriteLine($"{a} + {b} = {answer}");
+WriteLine("Press ENTER to end the APP");
+ReadLine();
