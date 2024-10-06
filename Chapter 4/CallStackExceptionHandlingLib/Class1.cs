286c7490806ea3e3c2ddb548a8c27a46e8c325a0
n1tuneyi <youssefhammam77@gmail.com>
2024-10-06 21:22:48 +0300

    Bad Functions that throw exception!

---
+++ Chapter 4/CallStackExceptionHandlingLib/Class1.cs
@@ -0,0 +1,20 @@
+﻿using static System.Console;
+
+namespace CallStackExceptionHandlingLib
+{
+    public class Calculator
+    {
+        public static void Gamma() 
+        {
+            WriteLine("In Gamma");
+            Delta();
+        }
+        private static void Delta()
+        
+        {
+            WriteLine("In Delta");
+            File.OpenText("bad file path");
+        }
+    }
+}
+
