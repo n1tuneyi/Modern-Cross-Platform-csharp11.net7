7c4fbfc13094e98691445aa56a4b5f074fd738f9
n1tuneyi <youssefhammam77@gmail.com>
2024-10-08 21:13:22 +0300

    Adding Elements in a collection of an object

--- Chapter 5/PeopleApp/Program.cs
+++ Chapter 5/PeopleApp/Program.cs
@@ -12,9 +12,13 @@ bob.DateOfBirth = new DateTime(1965, 12, 22);
 
 bob.FavoriteAncientWonder = WondersOfTheAncientWorld.StatueOfZeusAtOlympia;
 
-WriteLine(format: "{0}'s favorite wonder is {1}. Its integer is {2}.", arg0: bob.Name,
-          arg1: bob.FavoriteAncientWonder,
-          arg2: (int)bob.FavoriteAncientWonder);
+bob.BucketList = WondersOfTheAncientWorld.HangingGardensOfBabylon | WondersOfTheAncientWorld.MausoleumAtHalicarnassus;
+
+
+
+//WriteLine(format: "{0}'s favorite wonder is {1}. Its integer is {2}.", arg0: bob.Name,
+//          arg1: bob.FavoriteAncientWonder,
+//          arg2: (int)bob.FavoriteAncientWonder);
 
 
 Person alice = new()
@@ -23,4 +27,14 @@ Person alice = new()
     DateOfBirth = new(1998, 3, 7)
 };
 
-WriteLine(format: "{0} was born on {1:dd MMM yy}", arg0: alice.Name, arg1: alice.DateOfBirth);
+//WriteLine(format: "{0} was born on {1:dd MMM yy}", arg0: alice.Name, arg1: alice.DateOfBirth);
+
+bob.Children.Add(new Person { Name = "Alfred" }); 
+bob.Children.Add(new() { Name = "Zoe" }); 
+
+WriteLine($"{bob.Name} has {bob.Children.Count} children:");
+
+foreach (Person p in bob.Children)
+{
+    WriteLine($"> {p.Name}");
+}
