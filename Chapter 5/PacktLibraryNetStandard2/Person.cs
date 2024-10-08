f1c1060eabce31f439cb08cba0b366d0eece0bb4
n1tuneyi <youssefhammam77@gmail.com>
2024-10-08 21:12:47 +0300

    Added Collection as a Field

--- Chapter 5/PacktLibraryNetStandard2/Person.cs
+++ Chapter 5/PacktLibraryNetStandard2/Person.cs
@@ -5,5 +5,11 @@ public class Person: object
     public string? Name;
 
     public DateTime DateOfBirth;
+
+    public WondersOfTheAncientWorld FavoriteAncientWonder;
+
+    public WondersOfTheAncientWorld BucketList;
+
+    public List<Person> Children = new();
 }
 
