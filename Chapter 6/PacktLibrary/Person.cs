22dff2a7709461f8972b08f4dd080704dd8dc4eb
n1tuneyi <youssefhammam77@gmail.com>
2024-10-09 21:07:11 +0300

    Implement IComparable<T> interface

--- Chapter 6/PacktLibrary/Person.cs
+++ Chapter 6/PacktLibrary/Person.cs
@@ -1,5 +1,5 @@
 ﻿namespace Packt.Shared;
-public class Person
+public class Person: IComparable<Person?>
 {
     public string? Name { get; set; }
     public DateTime DateOfBirth { get; set; }
@@ -21,4 +21,42 @@ public class Person
             Shout?.Invoke(this, EventArgs.Empty);
         }
     }
+
+    public int CompareTo(Person? other)
+    {
+        int position;
+        if ((this is not null) && (other is not null))
+        {
+            if ((Name is not null) && (other.Name is not null))
+            {
+                position = Name.CompareTo(other.Name);
+            }
+            else if ((Name is not null) && (other.Name is null))
+            {
+                position = -1; 
+            }
+            else if ((Name is null) && (other.Name is not null))
+            {
+                position = 1; 
+            }
+            else
+            {
+                position = 0; 
+            }
+        }
+        else if ((this is not null) && (other is null))
+        {
+            position = -1; 
+        }
+        else if ((this is null) && (other is not null))
+        {
+            position = 1; 
+        }
+        else
+        {
+            position = 0; 
+        }
+        return position;
+    }
 }
+
