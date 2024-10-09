43f834897aaf25fe67d310152603baa3aa1ae6de
n1tuneyi <youssefhammam77@gmail.com>
2024-10-09 10:29:50 +0300

    Required modifier on properties

---
+++ Chapter 5/PacktLibraryModern/Book.cs
@@ -0,0 +1,19 @@
+﻿namespace Packt.Shared;
+using System.Diagnostics.CodeAnalysis;
+public class Book
+{
+    public required string? Isbn { get; set; }
+    public required string? Title { get; set; }
+    public string? Author { get; set; }
+    public int PageCount { get; set; }
+
+    public Book() { }
+
+    [SetsRequiredMembers]
+    public Book(string? isbn, string? title)
+    {
+        Isbn = isbn;
+        Title = title;
+    }
+}
+
