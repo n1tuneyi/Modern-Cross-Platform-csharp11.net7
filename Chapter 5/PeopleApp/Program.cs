9fc24f59c74a46cacf54216087f3fe0dced2d7c2
n1tuneyi <youssefhammam77@gmail.com>
2024-10-09 08:39:44 +0300

    Deconstructing an object

--- Chapter 5/PeopleApp/Program.cs
+++ Chapter 5/PeopleApp/Program.cs
@@ -46,45 +46,52 @@ BankAccount jonesAccount = new();
 jonesAccount.AccountName = "Mrs. Jones";
 jonesAccount.Balance = 2400;
 
-WriteLine(format: "{0} earned {1:C} interest.",arg0: jonesAccount.AccountName, 
-          arg1: jonesAccount.Balance * BankAccount.InterestRate);
+//WriteLine(format: "{0} earned {1:C} interest.",arg0: jonesAccount.AccountName, 
+//          arg1: jonesAccount.Balance * BankAccount.InterestRate);
 
 BankAccount gerrierAccount = new();
 gerrierAccount.AccountName = "Ms. Gerrier";
 gerrierAccount.Balance = 98;
 
-WriteLine(format: "{0} earned {1:C} interest.", arg0: gerrierAccount.AccountName,
-          arg1: gerrierAccount.Balance * BankAccount.InterestRate);
+//WriteLine(format: "{0} earned {1:C} interest.", arg0: gerrierAccount.AccountName,
+//          arg1: gerrierAccount.Balance * BankAccount.InterestRate);
 
 Person blankPerson = new();
-WriteLine(format: "{0} of {1} was created at {2:hh:mm:ss} on a {2:dddd}.", arg0: blankPerson.Name,
-          arg1: blankPerson.HomePlanet,
-          arg2: blankPerson.Instantiated);
+//WriteLine(format: "{0} of {1} was created at {2:hh:mm:ss} on a {2:dddd}.", arg0: blankPerson.Name,
+//          arg1: blankPerson.HomePlanet,
+//          arg2: blankPerson.Instantiated);
 
 Person gunny = new(initialName: "Gunny", homePlanet: "Mars");
 WriteLine(format: "{0} of {1} was created at {2:hh:mm:ss} on a {2:dddd}.", arg0: gunny.Name, arg1: gunny.HomePlanet,
 arg2: gunny.Instantiated);
 
-bob.WriteToConsole();
+//bob.WriteToConsole();
 
-WriteLine(bob.GetOrigin());
+//WriteLine(bob.GetOrigin());
 
-(string, int) fruit = bob.GetFruit();
+//(string, int) fruit = bob.GetFruit();
 
-WriteLine($"{fruit.Item1}, {fruit.Item2} there are.");
+//WriteLine($"{fruit.Item1}, {fruit.Item2} there are.");
 
-var fruitNamed = bob.GetNamedFruit();
+//var fruitNamed = bob.GetNamedFruit();
 
-WriteLine($"There are {fruitNamed.Number} {fruitNamed.Name}.");
+//WriteLine($"There are {fruitNamed.Number} {fruitNamed.Name}.");
 
-var thing1 = ("Neville", 4);
-WriteLine($"{thing1.Item1} has {thing1.Item2} children.");
+//var thing1 = ("Neville", 4);
+//WriteLine($"{thing1.Item1} has {thing1.Item2} children.");
 
-var thing2 = (bob.Name, bob.Children.Count);
-WriteLine($"{thing2.Name} has {thing2.Count} children.");
+//var thing2 = (bob.Name, bob.Children.Count);
+//WriteLine($"{thing2.Name} has {thing2.Count} children.");
 
 
-(string fruitName, int fruitNumber) = bob.GetFruit();
+//(string fruitName, int fruitNumber) = bob.GetFruit();
 
-WriteLine($"Deconstructed: {fruitName}, {fruitNumber}");
+//WriteLine($"Deconstructed: {fruitName}, {fruitNumber}");
 
+var (name1, dob1) = bob;
+
+WriteLine($"Deconstructed: {name1}, {dob1}");
+
+var (name2, dob2, fav2) = bob;
+
+WriteLine($"Deconstructed: {name2}, {dob2}, {fav2}");
