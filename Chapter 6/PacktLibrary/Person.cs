3cc6f2936d2538c0928213eb58a871c31efd2e04
n1tuneyi <youssefhammam77@gmail.com>
2024-10-09 20:49:48 +0300

    EventHandler and Event keyword

---
+++ Chapter 6/PacktLibrary/Person.cs
@@ -0,0 +1,24 @@
+﻿namespace Packt.Shared;
+public class Person
+{
+    public string? Name { get; set; }
+    public DateTime DateOfBirth { get; set; }
+
+    public void WriteToConsole()
+    {
+        WriteLine($"{Name} was born on a {DateOfBirth:dddd}.");
+    }
+
+    public int AngerLevel;
+
+    public event EventHandler? Shout;
+
+    public void Poke()
+    {
+        AngerLevel++;
+        if (AngerLevel >= 3)
+        {
+            Shout?.Invoke(this, EventArgs.Empty);
+        }
+    }
+}
