a0eaabee6f9e0db522ad9f8c3e2e5cf16aacdf5d
n1tuneyi <youssefhammam77@gmail.com>
2024-10-09 20:50:06 +0300

    Assign delegates

--- Chapter 6/PeopleApp/Program.cs
+++ Chapter 6/PeopleApp/Program.cs
@@ -17,5 +17,23 @@ lookupObject.Add(key: harry, value: "Delta");
 
 int key = 2;
 
-WriteLine(format: "Key {0} has value: {1}", arg0: key, arg1: lookupObject[key]);
-WriteLine(format: "Key {0} has value: {1}", arg0: harry, arg1: lookupObject[harry]);
+//WriteLine(format: "Key {0} has value: {1}", arg0: key, arg1: lookupObject[key]);
+//WriteLine(format: "Key {0} has value: {1}", arg0: harry, arg1: lookupObject[harry]);
+
+Dictionary<int, string> lookupIntString = new();
+lookupIntString.Add(key: 1, value: "Alpha");
+lookupIntString.Add(key: 2, value: "Beta");
+lookupIntString.Add(key: 3, value: "Gamma");
+lookupIntString.Add(key: 4, value: "Delta");
+
+key = 3;
+//WriteLine(format: "Key {0} has value: {1}", arg0: key, arg1: lookupIntString[key]);
+
+// Has to be += or -= because Shout is preceded with event keyword
+harry.Shout += Harry_Shout;
+harry.Shout += Harry_Shout2;
+
+harry.Poke();
+harry.Poke();
+harry.Poke();
+harry.Poke();
