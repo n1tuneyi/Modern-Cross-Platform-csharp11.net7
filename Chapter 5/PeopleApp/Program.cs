69e17bca32490c11762a1ab0503ba6ebcf2f2b23
n1tuneyi <youssefhammam77@gmail.com>
2024-10-09 11:40:26 +0300

    Alternative ways to pattern matching on object

--- Chapter 5/PeopleApp/Program.cs
+++ Chapter 5/PeopleApp/Program.cs
@@ -239,19 +239,31 @@ Passenger[] passengers = [
     new FirstClassPassenger { AirMiles = 1_419, Name = "Suman" },
     new FirstClassPassenger { AirMiles = 16_562, Name = "Lucy" },
     new BusinessClassPassenger { Name = "Janice" },
-    new CoachClassPassenger { CarryOnKG = 25.7, Name = "Dave" },new CoachClassPassenger { CarryOnKG = 0, Name = "Amit" },
+    new CoachClassPassenger { CarryOnKG = 25.7, Name = "Dave" },
+    new CoachClassPassenger { CarryOnKG = 0, Name = "Amit" },
 ];
 
 foreach (Passenger passenger in passengers)
 {
     decimal flightCost = passenger switch
     {
-        FirstClassPassenger p when p.AirMiles > 35000 => 1500M,
-        FirstClassPassenger p when p.AirMiles > 15000 => 1750M,
-        FirstClassPassenger _ => 2000M,
-        BusinessClassPassenger _ => 1000M,
+        //FirstClassPassenger p when p.AirMiles > 35000 => 1500M,
+        //FirstClassPassenger p when p.AirMiles > 15000 => 1750M,
+        //FirstClassPassenger _ => 2000M,
+        //FirstClassPassenger p => p.AirMiles switch
+        //{
+        //    > 35000 => 1500M,
+        //    > 15000 => 1750M,
+        //    _ => 2000M
+        //},
+
+        FirstClassPassenger { AirMiles: > 35000 } => 1500,
+        FirstClassPassenger { AirMiles: > 15000 } => 1750M,
+        FirstClassPassenger => 2000M,
+
+        BusinessClassPassenger => 1000M,
         CoachClassPassenger p when p.CarryOnKG < 10.0 => 500M,
-        CoachClassPassenger _ => 650M,
+        CoachClassPassenger => 650M,
         _ => 800M
     };
     WriteLine($"Flight costs {flightCost:C} for {passenger}");
