5d80c0803ec4df4e05c879cd8e9ea6f6947513b7
n1tuneyi <youssefhammam77@gmail.com>
2024-10-10 10:19:43 +0300

    Treat warnings as errors for better code write

---
+++ Chapter 6/WarningsAsErrors/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Enter a name: ");
+
+string name = Console.ReadLine()!;
+string? s = null;
+
+if(name == null)
+{
+    Console.WriteLine("You didn't enter a name");
+    return;
+}
+
+Console.WriteLine($"Hello, {name} has {name.Length} characters!");
