15ea9b4e9696c2e1084e7f49cba3bd464df0a15a
n1tuneyi <youssefhammam77@gmail.com>
2024-10-13 18:03:59 +0300

    Native sized integers

---
+++ Chapter 7/AssembliesAndNamespaces/Program.cs
@@ -0,0 +1,13 @@
+﻿using System.Xml.Linq;
+using System;
+
+XDocument doc = new();
+
+
+string s1 = "Hello";
+String s2 = "World";
+WriteLine($"{s1} {s2}");
+
+
+WriteLine($"int.MaxValue = {int.MaxValue:N0}");
+WriteLine($"nint.MaxValue = {nint.MaxValue:N0}");
