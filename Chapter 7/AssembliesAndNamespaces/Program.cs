87dff3d43a5cd6c1f8ee976ced0c6a84377e31ab
n1tuneyi <youssefhammam77@gmail.com>
2024-10-14 09:46:56 +0300

    Referencing the Uploaded NuGet Package

--- Chapter 7/AssembliesAndNamespaces/Program.cs
+++ Chapter 7/AssembliesAndNamespaces/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System.Xml.Linq;
 using System;
+using Packt.Shared;
 
 XDocument doc = new();
 
@@ -11,3 +12,17 @@ WriteLine($"{s1} {s2}");
 
 WriteLine($"int.MaxValue = {int.MaxValue:N0}");
 WriteLine($"nint.MaxValue = {nint.MaxValue:N0}");
+
+
+Write("Enter a color value in hex: ");
+string? hex = ReadLine();
+WriteLine("Is {0} a valid color value? {1}",
+arg0: hex, arg1: hex.IsValidHex());
+Write("Enter a XML element: ");
+string? xmlTag = ReadLine();
+WriteLine("Is {0} a valid XML element? {1}",
+arg0: xmlTag, arg1: xmlTag.IsValidXmlTag());
+Write("Enter a password: ");
+string? password = ReadLine(); 
+WriteLine("Is {0} a valid password? {1}",
+arg0: password, arg1: password.IsValidPassword());
