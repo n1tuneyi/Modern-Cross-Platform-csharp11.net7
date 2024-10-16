314011437c829b145a9b530452777f73f289ea27
n1tuneyi <youssefhammam77@gmail.com>
2024-10-16 09:52:32 +0300

    Random.Shared instance

--- Chapter 8/WorkingWithNumbers/Program.cs
+++ Chapter 8/WorkingWithNumbers/Program.cs
@@ -1,23 +1,33 @@
-﻿using System.Numerics;
+﻿//WriteLine("Working with large integers:");
+//WriteLine("-----------------------------------");
 
-WriteLine("Working with large integers:");
-WriteLine("-----------------------------------");
+//ulong big = ulong.MaxValue;
+//WriteLine($"{big,40:N0}");
 
-ulong big = ulong.MaxValue;
-WriteLine($"{big,40:N0}");
-
-BigInteger bigger = BigInteger.Parse("123456789012345678901234567890");
-WriteLine($"{bigger,40:N0}");
+//BigInteger bigger = BigInteger.Parse("123456789012345678901234567890");
+//WriteLine($"{bigger,40:N0}");
 
 
-WriteLine("Working with complex numbers:");
-Complex c1 = new(real: 4, imaginary: 2);
-Complex c2 = new(real: 3, imaginary: 7);
-Complex c3 = c1 + c2;
+//WriteLine("Working with complex numbers:");
+//Complex c1 = new(real: 4, imaginary: 2);
+//Complex c2 = new(real: 3, imaginary: 7);
+//Complex c3 = c1 + c2;
 
-WriteLine($"{c1} added to {c2} is {c3}");
+//WriteLine($"{c1} added to {c2} is {c3}");
 
-WriteLine("{0} + {1}i added to {2} + {3}i is {4} + {5}i",
-        c1.Real, c1.Imaginary,
-        c2.Real, c2.Imaginary,
-        c3.Real, c3.Imaginary);
+//WriteLine("{0} + {1}i added to {2} + {3}i is {4} + {5}i",
+//        c1.Real, c1.Imaginary,
+//        c2.Real, c2.Imaginary,
+//        c3.Real, c3.Imaginary);
+
+
+//Random r = new();
+
+//Random r = new(Seed: 46378);
+
+Random r = Random.Shared;
+
+int dieRoll = r.Next(minValue: 1, maxValue: 7);
+double randomReal = r.NextDouble();
+byte[] arrayOfBytes = new byte[256];
+r.NextBytes(arrayOfBytes);
