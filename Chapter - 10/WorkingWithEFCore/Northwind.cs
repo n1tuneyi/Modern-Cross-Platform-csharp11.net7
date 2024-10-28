f5217921e2d2b8dce61a1b01a83de339a5f7a1bb
n1tuneyi <youssefhammam77@gmail.com>
2024-10-28 14:45:41 +0300

    Chapter 10

---
+++ Chapter - 10/WorkingWithEFCore/Northwind.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Diagnostics;
+namespace Packt.Shared;
+
+public class Northwind : DbContext
+{
+    public DbSet<Category>? Categories { get; set; }
+    public DbSet<Product>? Products { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        string path = Path.Combine(Environment.CurrentDirectory, "Northwind.db");
+        string connection = $"Filename={path}";
+        ConsoleColor previousColor = ForegroundColor;
+        ForegroundColor = ConsoleColor.DarkYellow;
+        WriteLine($"Connection: {connection}");
+        ForegroundColor = previousColor;
+        optionsBuilder.UseSqlite(connection);
+
+        optionsBuilder.LogTo(WriteLine, new[] { RelationalEventId.CommandExecuting })
+                      .EnableSensitiveDataLogging();
+
+        optionsBuilder.UseLazyLoadingProxies();
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Category>()
+        .Property(category => category.CategoryName)
+        .IsRequired()
+        .HasMaxLength(15);
+
+        modelBuilder.Entity<Product>()
+        .HasQueryFilter(p => !p.Discontinued);
+
+        if (Database.ProviderName?.Contains("Sqlite") ?? false)
+        {
+            modelBuilder.Entity<Product>()
+            .Property(product => product.Cost)
+            .HasConversion<double>();
+        }
+    }
+}
