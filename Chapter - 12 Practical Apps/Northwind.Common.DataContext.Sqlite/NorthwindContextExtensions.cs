8d84f0d6cae1c4497aeb6c7c889d781854bfdf9d
n1tuneyi <youssefhammam77@gmail.com>
2024-11-03 11:14:32 +0200

    Chapter 12

---
+++ Chapter - 12 Practical Apps/Northwind.Common.DataContext.Sqlite/NorthwindContextExtensions.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Packt.Shared;
+public static class NorthwindContextExtensions
+{
+    public static IServiceCollection AddNorthwindContext(this IServiceCollection services, string relativePath = "..")
+    {
+        string databasePath = Path.Combine(relativePath, "Northwind.db");
+
+        services.AddDbContext<NorthwindContext>(options =>
+        {
+            options.UseSqlite($"Data Source={databasePath}");
+            options.LogTo(Console.WriteLine, new[] { Microsoft.EntityFrameworkCore
+                                            .Diagnostics.RelationalEventId.CommandExecuting });
+        });
+        return services;
+    }
+}
---
+++ Chapter - 12 Practical Apps/Northwind.Common.UnitTests/UnitTest1.cs
@@ -0,0 +1,26 @@
+using Packt.Shared;
+
+namespace Northwind.Common.UnitTests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void DatabaseConnectTest()
+    {
+        using (NorthwindContext db = new())
+        {
+            Assert.True(db.Database.CanConnect());
+        }
+    }
+
+    [Fact]
+    public void CategoryCountTest()
+    {
+        using (NorthwindContext db = new())
+        {
+            int expected = 8;
+            int actual = db.Categories.Count();
+            Assert.Equal(expected, actual);
+        }
+    }
+}
