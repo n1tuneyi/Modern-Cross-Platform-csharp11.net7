916cd83a31cf7abbb39106c7abfc4a54e3db5ccd
n1tuneyi <youssefhammam77@gmail.com>
2024-11-03 11:19:29 +0200

    Chapter 11

---
+++ Chapter - 11/LinqWithEFCore/Program.cs
@@ -0,0 +1,18 @@
+﻿//FilterAndSort();
+
+//var anonymouslyTypedObject = new
+//{
+//    Name = "Boris Johnson",
+//    DateOfBirth = new DateTime(year: 1964, month: 6, day: 19),
+//};
+
+//JoinCategoriesAndProducts();
+
+//GroupJoinCategoriesAndProducts();
+//AggregateProducts();
+//PagingProducts();
+//CustomExtensionMethods();
+
+//OutputProductsAsXml();
+
+ProcessSettings();
